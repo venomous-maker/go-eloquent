de1e3e85a2c78a69fa298bb05672287966aa7278
venomous-maker <morganokumu8+1@gmail.com>
2025-10-08 22:00:24 +0300

    Added route helpers

---
+++ Routes/Helpers/helpers.routes.go
@@ -0,0 +1,13 @@
+package EloquentRouteHelpers
+
+import "github.com/gin-gonic/gin"
+
+// ExtraRoute represents a single route with its method, path, handler, middleware, optional subgroup, and nested routes.
+type ExtraRoute struct {
+	Method     string
+	Path       string
+	Handler    gin.HandlerFunc
+	Middleware []gin.HandlerFunc
+	Children   []ExtraRoute // ⬅ nested routes
+	Group      string       // ⬅ optional subgroup prefix
+}
